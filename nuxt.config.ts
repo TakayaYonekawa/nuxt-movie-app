403fa9d987cd2f311f149ba3776036fc95934fd7
米川貴也 <zuomujianwu1@gmail.com>
2023-10-29 11:56:56 +0900

    練習用コミット

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,12 +8,6 @@ export default defineNuxtConfig({
   devtools: { 
     enabled: true
    },
-   runtimeConfig: {
-    apiKey: process.env.NUXT_API_KEY,
-    apiBaseUrl: process.env.NUXT_API_BASE_URL,
-    public: {
-      imgBaseUrl: ''
-    }
-   }
+
 
 })
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,6 @@
     <div class="flex justify-center items-center h-32">
       <input
         class="px-2 py-1 border-gray-800 rounded-md min-w-64"
-        v-model="searchTerm"
         placeholder="Search..."
         type="text"
       />
@@ -14,22 +13,19 @@
     <div
       class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 self-center gap-x-10 gap-y-10 mb-10"
     >
-      <div v-for="movie in data?.results" :key="movie.title">
-        <MovieCard :movie="movie"></MovieCard>
+      <div >
+        <MovieCard ></MovieCard>
       </div>
     </div>
     <div class="flex justify-center">
       <button
-        v-if="!disabledPrevious"
-        @click="page--"
         class="px-4 py-2 text-m border rounded-lg"
       >
         Previoud
       </button>
-      <div class="px-4 py-2 text-m border rounded-lg">{{ page }}</div>
+      <div class="px-4 py-2 text-m border rounded-lg"></div>
       <button
-        v-if="!disabledNext"
-        @click="page++"
+
         class="px-4 py-2 text-m border rounded-lg"
       >
         Next
@@ -39,23 +35,5 @@
 </template>
 
 <script setup lang="ts">
-import { type APIResponse } from "~/types/APIResponse";
 
-const searchTerm = ref("");
-const page = ref(1);
-
-const disabledPrevious = computed(() => {
-  return page.value === 1;
-});
-
-const disabledNext = computed(() => {
-  return page.value + 1 === data.value?.total_pages;
-});
-
-const debouncedSearchTerm = refDebounced(searchTerm, 700);
-
-const url = computed(() => {
-  return `api/movies/search?query=${debouncedSearchTerm.value}&page=${page.value}`;});
-
-const { data } = await useFetch<APIResponse>(url);
 </script>
